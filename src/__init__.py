1c10f4d80996294cbc96267ade014ff7bf0bef8f
miniBear <ax_zaa@windowslive.com>
2023-02-28 20:09:14 +0700

    INITIAL

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+import man_mirror as mmr
+
+
+if( __init__.__name__ == '__main__'): 
+    mmr.getJson(10, 1, 2)
---
+++ src/man_mirror.py
@@ -0,0 +1,6 @@
+
+host = 'https://www.manmirror.net'
+
+def getJson(postId, chapter, page ): 
+    url = 'https://www.manmirror.net/test/#{id}/#{chapter}/#{page}.json'
+    print(url)
