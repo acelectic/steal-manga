abffa03d893fabaa53607010ad64a20432ea43a8
miniBear <ax_zaa@windowslive.com>
2023-03-01 05:18:34 +0700

    Fix: refactor

---
+++ utils/pdf_helper.py
@@ -0,0 +1,18 @@
+""" pdf helper """
+from typing import List
+from PIL import Image
+
+
+def merge_images_to_pdf(image_paths: List[str], output: str):
+    """ merge images to pdf """
+    first_image = None
+    rest_images = []
+    for i, image_path in enumerate(sorted(image_paths, key=lambda x: int(x.split('/')[-1].replace('.png', '')))):
+        img = Image.open(image_path).convert('RGB')
+        if i == 0:
+            first_image = img
+        else:
+            rest_images.append(img)
+
+    if first_image:
+        first_image.save(output, save_all=True, append_images=rest_images)
