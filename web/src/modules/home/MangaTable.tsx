a75ea1d8a7cc06694f503d4368cfc121c3634bcf
miniBear <ax_zaa@windowslive.com>
2023-06-25 22:08:05 +0700

    fix: add table sort, search, filter

--- web/src/modules/home/MangaTable.tsx
+++ web/src/modules/home/MangaTable.tsx
@@ -14,11 +14,14 @@ import {
   message,
 } from 'antd'
 import { IGetMangaUpdatedResponse } from '../../service/manga-updated/types'
-import React, { useContext, useEffect, useMemo, useRef, useState } from 'react'
+import React, { useCallback, useContext, useEffect, useMemo, useRef, useState } from 'react'
 import { ColumnType } from 'antd/es/table'
 import { css } from '@emotion/css'
 import { updateMangeConfig } from '../../service/manga-updated'
 import { chain } from 'lodash'
+import { FilterConfirmProps, FilterValue } from 'antd/es/table/interface'
+import { SearchOutlined } from '@ant-design/icons'
+import Highlighter from 'react-highlight-words'
 
 const warpCss = css`
   width: 100%;
@@ -49,6 +52,8 @@ interface IItem {
   disabled: boolean
   downloaded: number
 }
+
+type DataIndex = keyof IItem
 
 interface IEditableRowProps {
   index: number
@@ -185,26 +190,144 @@ interface IMangaTableProps {
 export const MangaTable = (props: IMangaTableProps) => {
   const { title, data } = props
   const [dataSource, setDataSource] = useState<IItem[]>([])
+  const [filteredValue, setFilteredValue] = useState<FilterValue>([])
+
+  const [searchText, setSearchText] = useState('')
+  const [searchedColumn, setSearchedColumn] = useState('')
+  const searchInput = useRef<InputRef>(null)
+
+  const handleSearch = useCallback(
+    (
+      selectedKeys: string[],
+      confirm: (param?: FilterConfirmProps) => void,
+      dataIndex: DataIndex,
+    ) => {
+      confirm()
+      setSearchText(selectedKeys[0])
+      setSearchedColumn(dataIndex)
+    },
+    [],
+  )
+
+  const handleReset = useCallback((clearFilters: () => void) => {
+    clearFilters()
+    setSearchText('')
+  }, [])
 
   useEffect(() => {
-    setDataSource(
-      data.map(
-        (e): IItem => ({
-          cartoonName: e.cartoonName,
-          cartoonId: e.cartoonId,
-          latestChapter: e.latestChapter,
-          maxChapter: +e.maxChapter,
-          disabled: e.disabled,
-          downloaded: e.downloaded,
-        }),
-      ),
+    const _data = data.map(
+      (e): IItem => ({
+        cartoonName: e.cartoonName,
+        cartoonId: e.cartoonId,
+        latestChapter: e.latestChapter,
+        maxChapter: +e.maxChapter,
+        disabled: e.disabled,
+        downloaded: e.downloaded,
+      }),
     )
+    setDataSource(chain(_data).orderBy(['cartoonName'], ['asc']).value())
   }, [data])
+
+  const getColumnSearchProps = useCallback(
+    (dataIndex: DataIndex): ColumnType<IItem> => ({
+      filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+        <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+          <Input
+            ref={searchInput}
+            placeholder={`Search ${dataIndex}`}
+            value={selectedKeys[0]}
+            onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+            onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+            style={{ marginBottom: 8, display: 'block' }}
+          />
+          <Space>
+            <Button
+              type="primary"
+              onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+              icon={<SearchOutlined />}
+              size="small"
+              style={{ width: 90 }}
+            >
+              Search
+            </Button>
+            <Button
+              onClick={() => clearFilters && handleReset(clearFilters)}
+              size="small"
+              style={{ width: 90 }}
+            >
+              Reset
+            </Button>
+            <Button
+              type="link"
+              size="small"
+              onClick={() => {
+                confirm({ closeDropdown: false })
+                setSearchText((selectedKeys as string[])[0])
+                setSearchedColumn(dataIndex)
+              }}
+            >
+              Filter
+            </Button>
+            <Button
+              type="link"
+              size="small"
+              onClick={() => {
+                close()
+              }}
+            >
+              close
+            </Button>
+          </Space>
+        </div>
+      ),
+      filterIcon: (filtered: boolean) => (
+        <SearchOutlined style={{ color: filtered ? '#1677ff' : undefined }} />
+      ),
+      onFilter: (value, record) =>
+        record[dataIndex]
+          .toString()
+          .toLowerCase()
+          .includes((value as string).toLowerCase()),
+      onFilterDropdownOpenChange: (visible) => {
+        if (visible) {
+          setTimeout(() => searchInput.current?.select(), 100)
+        }
+      },
+      render: (text) =>
+        searchedColumn === dataIndex ? (
+          <Highlighter
+            highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+            searchWords={[searchText]}
+            autoEscape
+            textToHighlight={text ? text.toString() : ''}
+          />
+        ) : (
+          text
+        ),
+    }),
+    [handleReset, handleSearch, searchText, searchedColumn],
+  )
 
   const defaultColumns = useMemo(
     (): (ColumnType<IItem> & Partial<Pick<EditableCellProps, 'editable' | 'dataType'>>)[] => [
-      { title: 'Cartoon Name', key: 'cartoonName', dataIndex: 'cartoonName' },
-      { title: 'Cartoon Id', key: 'cartoonId', dataIndex: 'cartoonId' },
+      {
+        title: 'Cartoon Name',
+        key: 'cartoonName',
+        dataIndex: 'cartoonName',
+        width: 400,
+        sorter: (a, b) => {
+          if (a.cartoonName.toLowerCase() < b.cartoonName.toLowerCase()) return -1
+          if (a.cartoonName.toLowerCase() > b.cartoonName.toLowerCase()) return 1
+          return 0
+        },
+        ...getColumnSearchProps('cartoonName'),
+      },
+      {
+        title: 'Cartoon Id',
+        key: 'cartoonId',
+        dataIndex: 'cartoonId',
+        width: 250,
+      },
       {
         title: 'Latest Chapter',
         key: 'latestChapter',
@@ -251,7 +374,7 @@ export const MangaTable = (props: IMangaTableProps) => {
         },
       },
     ],
-    [title],
+    [getColumnSearchProps, title],
   )
 
   const handleSave = (row: IItem) => {
@@ -299,6 +422,9 @@ export const MangaTable = (props: IMangaTableProps) => {
             cell: EditableCell,
           },
         }}
+        size="small"
+        showSorterTooltip
+        bordered
       />
     </Space>
   )
