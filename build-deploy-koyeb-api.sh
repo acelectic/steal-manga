23fb7288ec9290a67b339391b033c98c46aab0ef
miniBear <ax_zaa@windowslive.com>
2023-09-09 07:07:25 +0700

    fix: edit script deploy api to koyeb

--- build-deploy-koyeb-api.sh
+++ build-deploy-koyeb-api.sh
@@ -1,6 +1,7 @@
 cd api \
 && docker buildx build -f Dockerfile -t acelectic/steal-manga:api . \
 && docker push acelectic/steal-manga:api \
+&& koyeb service resume api/steal-manga-api \
 && koyeb service redeploy api/steal-manga-api \
 && cd ..
 
