2c0eedae86d2e185daf8243d3660db7fd2b998db
miniBear <ax_zaa@windowslive.com>
2023-11-05 08:37:51 +0700

    fix: edit bash script

--- build-api.sh
+++ build-api.sh
@@ -1,6 +1,23 @@
+#!/usr/bin/env bash
+source ./bash-flag-script.sh
+
 cd api \
 && docker buildx build -f Dockerfile -t acelectic/steal-manga:api . \
-&& docker push acelectic/steal-manga:api \
 && cd ..
+
+
+if [ $push_image ]:
+then 
+echo "push_image: $push_image"
+docker push acelectic/steal-manga:api
+fi
+
+
+if [ $deploy_service ]:
+then 
+echo "deploy_service: $deploy_service"
+docker-compose stop api \
+    && docker-compose up -d --force-recreate api
+fi
 
 # && koyeb service redeploy steal-manga-web/api \
--- build-web.sh
+++ build-web.sh
@@ -1,6 +1,22 @@
+#!/usr/bin/env bash
+source ./bash-flag-script.sh
+
 cd web \
 && docker buildx build -f Dockerfile -t acelectic/steal-manga:web . \
-&& docker push acelectic/steal-manga:web \
 && cd ..
+
+if [ $push_image ];
+then 
+echo "push_image: $push_image"
+docker push acelectic/steal-manga:web
+fi
+
+
+if [ $deploy_service ];
+then 
+echo "deploy_service: $deploy_service"
+docker-compose stop web \
+    && docker-compose up -d --force-recreate web
+fi
 
 # && koyeb service redeploy steal-manga-web/web \
